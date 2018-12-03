1a0e6b0a24acddb1c8a94873bd7168fe5c76b7bb
22wg <gwee1216@naver.com>
2018-12-03 16:14:09 +0900

    Handling Routes with Express
    Express를 이용해 라우팅처리

--- index.js
+++ index.js
@@ -7,4 +7,18 @@ function handleListening(){
     console.log(`Listening on: http://localhost:${PORT}`);
 }
 
+function handleRoot(req,res){
+    console.log("Hi from home!!");
+    console.log(req);
+    res.send('Hello, from home!!');
+}
+
+function handleProfile(req,res){
+    res.send("You are on my profile");
+}
+
+app.get("/", handleRoot);
+
+app.get("/profile", handleProfile);
+
 app.listen(PORT, handleListening);
