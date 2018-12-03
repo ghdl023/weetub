0e617fa491cc3f51d7a87062ef4185ced8a7e050
22wg <gwee1216@naver.com>
2018-12-03 17:17:29 +0900

    Middlewares Install & Usage
    Middlewares(morgan, helmet, cookie-parser, body-parser) Install & Usage

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // const express = require("express");
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
 const app = express();
 
 const PORT = 4000;
@@ -8,9 +13,22 @@ const handleListening = () =>
     console.log(`Listening on: http://localhost:${PORT}`);
 
 
-const handleRoot = (req,res)=> res.send('Hello, from my home!!');
+const handleRoot = (req, res)=> res.send('Hello, from my home!!');
 
-const handleProfile = (req,res)=> res.send("You are on my profile");
+const handleProfile = (req, res)=> res.send("You are on my profile");
+
+// const betweenHome = (req, res, next) => {
+//     console.log("Between");
+//     next();
+// }
+
+// app.use(betweenHome);
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(helmet());
+app.use(morgan("dev"));
 
 app.get("/", handleRoot);
 
