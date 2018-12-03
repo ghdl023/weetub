dac28c2116ed5dd428db2307a08acd338ea0532e
22wg <gwee1216@naver.com>
2018-12-03 16:41:06 +0900

    Update index.js
    ES6 Style Coding

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
-const express = require("express");
+// const express = require("express");
+import express from "express";
 const app = express();
 
 const PORT = 4000;
 
-function handleListening(){
+const handleListening = () =>
     console.log(`Listening on: http://localhost:${PORT}`);
-}
 
-function handleRoot(req,res){
-    console.log("Hi from home!!");
-    console.log(req);
-    res.send('Hello, from home!!');
-}
 
-function handleProfile(req,res){
-    res.send("You are on my profile");
-}
+const handleRoot = (req,res)=> res.send('Hello, from my home!!');
+
+const handleProfile = (req,res)=> res.send("You are on my profile");
 
 app.get("/", handleRoot);
 
