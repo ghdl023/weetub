5b1b8fa7765410a788132aef1d67f29b56ce0548
22wg <gwee1216@naver.com>
2018-12-04 15:59:02 +0900

    Update app.js
    routes를 import해와서 미들웨어등록에 사용

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import userRouter from "./routes/userRouter";
-import videoRouter from "./routes/videoRouter";
-import globalRouter from "./routes/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
+
 const app = express();
 
 // const betweenHome = (req, res, next) => {
@@ -22,8 +24,8 @@ app.use(bodyParser.urlencoded({extended : true}));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.use("/", globalRouter);
-app.use("/user", userRouter);
-app.use("/video", videoRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
