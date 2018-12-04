0af7889fc7e22c8954b26f9a1e6e32c35689ed0d
22wg <gwee1216@naver.com>
2018-12-04 15:57:52 +0900

    Create routes.js
    라우팅경로를 상수로 선언해놓고 routes 객체로 이용할것

---
+++ routes.js
@@ -0,0 +1,44 @@
+// Global
+
+const HOME = "/",
+    JOIN = "/join",
+    LOGIN = "/login",
+    LOGOUT = "/logout",
+    SEARCH = "/search";
+
+
+// Users
+
+const USERS = "/users",
+    USER_DETAIL = "/:id",
+    EDIT_PROFILE = "/:id/edit-profile",
+    CHANGE_PASSWORD = "/:id/change-password";
+
+
+// Videos
+
+const VIDEOS = "/videos",
+    UPLOAD = "/upload",
+    VIDEO_DETAIL = "/:id",
+    EDIT_VIDEO = "/:id/edit",
+    DELETE_VIDEO = "/:id/delete";
+
+
+const routes = {
+    home : HOME,
+    join : JOIN,
+    login : LOGIN,
+    logout : LOGOUT,
+    search : SEARCH,
+    users : USERS,
+    userDetail : USER_DETAIL,
+    editProfile : EDIT_PROFILE,
+    changePassword : CHANGE_PASSWORD,
+    videos : VIDEOS,
+    upload : UPLOAD,
+    videoDetail : VIDEO_DETAIL,
+    editVideo : EDIT_VIDEO,
+    deleteVideo : DELETE_VIDEO
+};
+
+export default routes;
