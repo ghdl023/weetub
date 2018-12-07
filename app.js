d69d087bf497d88b1627c50ba05580ce3a505c6a
22wg <gwee1216@naver.com>
2018-12-07 21:42:03 +0900

    Setting View Engine
    pug 세팅

--- app.js
+++ app.js
@@ -11,12 +11,7 @@ import routes from "./routes";
 
 const app = express();
 
-// const betweenHome = (req, res, next) => {
-//     console.log("Between");
-//     next();
-// }
-
-// app.use(betweenHome);
+app.set("view engine", "pug");
 
 app.use(cookieParser());
 app.use(bodyParser.json());
