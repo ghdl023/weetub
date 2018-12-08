5615eb78ca5ea8c488e0167c55e9887222e42ab2
22wg <gwee1216@naver.com>
2018-12-08 17:58:19 +0900

    Making Video Database

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+    {
+        id:324393,
+        title : "Video Awesome",
+        description : "This is something I love",
+        views : 24,
+        videoFile : "https://archive.org/details/BigBuckBunny_124",
+        creator : {
+            id : 121212,
+            name : "Nicolas",
+            email : "nico@las.com"
+        }
+    },
+    {
+        id:549123,
+        title : "Video Super",
+        description : "This is something I love",
+        views : 24,
+        videoFile : "https://archive.org/details/BigBuckBunny_124",
+        creator : {
+            id : 121212,
+            name : "Nicolas",
+            email : "nico@las.com"
+        }
+    },
+    {
+        id:123349,
+        title : "Video Hohoho",
+        description : "This is something I love",
+        views : 24,
+        videoFile : "https://archive.org/details/BigBuckBunny_124",
+        creator : {
+            id : 121212,
+            name : "Nicolas",
+            email : "nico@las.com"
+        }
+    },
+    {
+        id:324393,
+        title : "Video Awesome",
+        description : "This is something I love",
+        views : 24,
+        videoFile : "https://archive.org/details/BigBuckBunny_124",
+        creator : {
+            id : 121212,
+            name : "Nicolas",
+            email : "nico@las.com"
+        }
+    }
+];
