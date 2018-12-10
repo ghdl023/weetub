f7be3e31b426017fb3453a77ab4bad7bdcbaeb9b
22wg <gwee1216@naver.com>
2018-12-10 20:51:47 +0900

    Two Login Controller
    getLogin, postLogin

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,8 @@
-import routes from "../routes"
+import routes from "../routes";
 
 export const getJoin = (req, res) => {
     res.render("join", { pageTitle : 'Join'});
-}
+};
 
 export const postJoin = (req, res) => {
     // console.log(req.body);
@@ -17,9 +17,17 @@ export const postJoin = (req, res) => {
         // To Do : Log user in
         res.redirect(routes.home);
     }
-}
+};
 
-export const login = (req, res) => res.render("login", { pageTitle : 'Login'});
+export const getLogin = (req, res) => {
+    res.render("login", { pageTitle : 'Login'});
+};
+
+export const postLogin = (req, res) => {
+    res.redirect(routes.home);
+};
+
+
 export const logout = (req, res) => res.render("logout", { pageTitle : 'Logout'});
 export const users = (req, res) => res.render("users", { pageTitle : 'Users'});
 export const userDetail = (req, res) => res.render("userDetail", { pageTitle : 'User Detail'});
