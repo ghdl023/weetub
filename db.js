c6ab9190da1727e92b2ca3a8ee284e29e05a0b06
22wg <gwee1216@naver.com>
2018-12-14 12:51:58 +0900

    Connecting MongoDB
    with mongoose

--- db.js
+++ db.js
@@ -1,50 +1,73 @@
-export const videos = [
+import mongoose from "mongoose";
+
+mongoose.connect(
+    "mongodb://localhost:27017/we-tube",
     {
-        id:324393,
-        title : "Video Awesome",
-        description : "This is something I love",
-        views : 24,
-        videoFile : "https://www.youtube.com/embed/nTpaEw5f-OU",
-        creator : {
-            id : 121212,
-            name : "Nicolas",
-            email : "nico@las.com"
-        }
-    },
-    {
-        id:549123,
-        title : "Video Super",
-        description : "This is something I love",
-        views : 24,
-        videoFile : "https://www.youtube.com/embed/nTpaEw5f-OU",
-        creator : {
-            id : 121212,
-            name : "Nicolas",
-            email : "nico@las.com"
-        }
-    },
-    {
-        id:123349,
-        title : "Video Hohoho",
-        description : "This is something I love",
-        views : 24,
-        videoFile : "https://www.youtube.com/embed/nTpaEw5f-OU",
-        creator : {
-            id : 121212,
-            name : "Nicolas",
-            email : "nico@las.com"
-        }
-    },
-    {
-        id:775821,
-        title : "Video Awesome",
-        description : "This is something I love",
-        views : 24,
-        videoFile : "https://www.youtube.com/embed/nTpaEw5f-OU",
-        creator : {
-            id : 121212,
-            name : "Nicolas",
-            email : "nico@las.com"
-        }
+        useNewUrlParser : true,
+        useFindAndModify : false
     }
-];
+);
+
+const db = mongoose.connection;
+
+const handleOpen = () => {
+    console.log("Connected to DB");
+}
+
+const handleError = (err) => {
+    console.log(`Error on DB Connection:${error}!!`);
+}
+
+db.once("open", handleOpen);
+db.on("error", handleError);
+
+// export const videos = [
+//     {
+//         id:324393,
+//         title : "Video Awesome",
+//         description : "This is something I love",
+//         views : 24,
+//         videoFile : "https://www.youtube.com/embed/nTpaEw5f-OU",
+//         creator : {
+//             id : 121212,
+//             name : "Nicolas",
+//             email : "nico@las.com"
+//         }
+//     },
+//     {
+//         id:549123,
+//         title : "Video Super",
+//         description : "This is something I love",
+//         views : 24,
+//         videoFile : "https://www.youtube.com/embed/nTpaEw5f-OU",
+//         creator : {
+//             id : 121212,
+//             name : "Nicolas",
+//             email : "nico@las.com"
+//         }
+//     },
+//     {
+//         id:123349,
+//         title : "Video Hohoho",
+//         description : "This is something I love",
+//         views : 24,
+//         videoFile : "https://www.youtube.com/embed/nTpaEw5f-OU",
+//         creator : {
+//             id : 121212,
+//             name : "Nicolas",
+//             email : "nico@las.com"
+//         }
+//     },
+//     {
+//         id:775821,
+//         title : "Video Awesome",
+//         description : "This is something I love",
+//         views : 24,
+//         videoFile : "https://www.youtube.com/embed/nTpaEw5f-OU",
+//         creator : {
+//             id : 121212,
+//             name : "Nicolas",
+//             email : "nico@las.com"
+//         }
+//     }
+// ];
