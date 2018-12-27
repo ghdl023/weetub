27ec7f9da684145ce9e28b49647d4e9e08202f61
22wg <gwee1216@naver.com>
2018-12-27 14:55:21 +0900

    ESLint Add Rules
    linkbreak-style:off,
    prettier/prettier:off

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: ["airbnb-base", "plugin:prettier/recommended"],
   rules: {
     "no-console": "off",
-    "linebreak-style": "off"
+    "linebreak-style": "off",
+    "prettier/prettier": "off"
   }
 };
