c2be16f3c2bbae12ebaaab87cd3e6301da7b0280
gwee1216 <gwee1216@naver.com>
2019-01-14 16:26:42 +0900

    #7.3 Adding Creator to Video
    사용자가 업로드한 비디오에 사용자id 정보 추가하는 작업.
    
    .populate()는 ObjectId 타입에만 사용할수있다.

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -42,10 +42,12 @@ export const postUpload = async (req, res) => {
   const newVideo = await Video.create({
     fileUrl: path,
     title,
-    description
+    description,
+    creator: req.user.id
   });
-  console.log(newVideo);
-  // To Do : Upload and save video
+  // console.log(newVideo);
+  req.user.videos.push(newVideo.id);
+  req.user.save();
   res.redirect(routes.videoDetail(newVideo.id));
 };
 
@@ -55,7 +57,7 @@ export const videoDetail = async (req, res) => {
     params: { id }
   } = req;
   try {
-    const video = await Video.findById(id);
+    const video = await Video.findById(id).populate("creator");
     console.log(video);
     res.render("videoDetail", { pageTitle: video.title, video });
   } catch (error) {
--- models/Comment.js
+++ models/Comment.js
@@ -1,18 +1,18 @@
 import mongoose from "mongoose";
 
 const CommentSchema = new mongoose.Schema({
-    text: {
-        type: String,
-        required: "Text is required"
-    },
-    createAt: {
-        type: Date,
-        default: Date.now
-    },
-    video: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Video"
-    }
+  text: {
+    type: String,
+    required: "Text is required"
+  },
+  createAt: {
+    type: Date,
+    default: Date.now
+  },
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  }
 });
 
 const model = mongoose.model("Comment", CommentSchema);
